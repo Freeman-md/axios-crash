b3aa11f2832c3bfa58c6d991d3db23db79ebf9a0
Freeman-md <freemanmadudili@gmail.com>
2021-07-24 23:42:46 +0100

    Get Data Simultaneously

--- main.js
+++ main.js
@@ -38,7 +38,12 @@ function removeTodo() {
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos>limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/posts>limit=5')
+  ])
+  .then(axios.spread((todos, posts) => showOutput(posts)))
+  .catch(err => console.log(err))
 }
 
 // CUSTOM HEADERS
